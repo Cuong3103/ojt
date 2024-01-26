eed7440b4fe58373cac18a31aeeec959492fa24d
Tuong <tuong.wall123@gmail.com>
2024-01-26 12:55:35 +0700

    implement SyllabusCard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
-
-import Dropdown from "./components/Dropdown/Dropdown";
 import { useState } from "react";
+import SyllabusCard from "./components/SyllabusCard/SyllabusCard";
 
 export default function Home() {
   const [option, setOption] = useState('');
@@ -12,18 +11,7 @@ export default function Home() {
 
   return (
     <div>
-      <Dropdown
-        options={[
-          { label: 'Option 0', value: 'Option 0' },
-          { label: 'Option 1', value: 'Option 1' },
-          { label: 'Option 2', value: 'Option 2' },
-          { label: 'Option 3', value: 'Option 3' },
-        ]}
-        value={option}
-        onChange={handleOptionChange}
-        placeholder="Select an option"
-      />
-      <p>You choose {option}!</p>
+      <SyllabusCard />
 
 
     </div>
