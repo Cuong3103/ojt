84c32bf6d8f4eb1a4cb7d8b0b1501555aefc4171
Van Doi <doipvse161264@fpt.edu.vn>
2024-02-29 03:31:58 +0000

    feat/syllabus-detail-storybook: add syllabusDetail storybook

---
+++ src/stories/Detail.stories.tsx
@@ -0,0 +1,11 @@
+import { Detail } from "@/app/components/syllabus-detail/detail";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Detail> = { component: Detail };
+export default meta;
+
+type Story = StoryObj<typeof Detail>;
+
+export const Default: Story = {
+    args: {},
+};
