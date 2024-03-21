e8a6d8d0444b9e72ad955f0781ca9037fc758667
Nguyen Khoi Nguyen <nkhoinguyen26@gmail.com>
2024-03-21 13:58:50 +0700

    feat: update utils to convert date to timestamp

--- src/utils/formatUtils.ts
+++ src/utils/formatUtils.ts
@@ -1,7 +1,11 @@
 export const fromTimestampToDateString = (timestamp: number): string => {
   const date = new Date(timestamp * 1000);
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const year = date.getFullYear();
   return `${day}/${month}/${year}`;
+};
+
+export const fromDateToTimestamp = (date: Date): number => {
+  return Math.round(date.getTime() / 1000);
 };
