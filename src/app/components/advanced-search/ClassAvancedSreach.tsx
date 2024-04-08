99d43b20c4319e5908d28f252e3655c9767f81fb
Van Doi <doipvse161264@fpt.edu.vn>
2024-04-08 02:03:33 +0000

    Feat/modify create class

--- src/app/components/advanced-search/ClassAvancedSreach.tsx
+++ src/app/components/advanced-search/ClassAvancedSreach.tsx
@@ -2,6 +2,8 @@
 import Button from "../button/button";
 import { FC } from "react";
 import { toast } from "react-toastify";
+import AppTimeFramePicker from "../time-picker/time-frame-picker";
+import { InputSearch } from "../input-box/search-input";
 
 
 
@@ -13,7 +15,7 @@ type ClassAvancedSreachProps = {
 export const ClassAvancedSreach: FC<ClassAvancedSreachProps> = ({
     isOpenBox,
     handleOpenBox,
-}) => { 
+}) => {
     const handleAdvancedSearch = () => {
         handleOpenBox();
         toast.error("This feature is not implemented yet");
@@ -21,21 +23,151 @@ export const ClassAvancedSreach: FC<ClassAvancedSreachProps> = ({
 
 
     return (
-        isOpenBox &&(
-            <div className="box-border bg-slate-500 border rounded-xl">
-                Hello
-                <section className="flex justify-end items-center gap-4 px-5">
-          <Button
-            title="Cancel"
-            className=" w-[48px] h-[28px]  py-[2px] rounded-[8px] underline text-[#E74A3B] text-sm font-bold"
-            onClick={handleOpenBox}
-          />
-          <Button
-            title="Search"
-            className="btn bg-primary-color text-white hover:text-black"
-            onClick={handleAdvancedSearch}
-          />
-        </section>
+        isOpenBox && (
+            <div className="box-border  border rounded-xl ml-[15px]">
+                <div className="flex gap-20 ml-[10px]">
+                    <div>
+                        <p className="font-bold">Class location</p>
+                        <InputSearch />
+
+                    </div>
+                    <div>
+                        <p className="font-bold">Class time frame</p>
+                        <AppTimeFramePicker />
+
+                    </div>
+                </div>
+                <div className="flex items-center gap-20 w-ful mt-[30px]">
+                    <article className="flex gap-4 ml-[10px]">
+                        <p className="font-bold ">Class time</p>
+                        <div>
+                            <div className="form-control">
+                                <label className="cursor-pointer flex items-center gap-2 mb-2">
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked
+                                        className=""
+                                    />
+                                    <span className="label-text">Morning</span>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="cursor-pointer flex items-center gap-2 mb-2">
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked
+                                        className=""
+                                    />
+                                    <span className="label-text">Noon</span>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="cursor-pointer flex items-center gap-2 mb-2">
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked
+                                        className=""
+                                    />
+                                    <span className="label-text">Night</span>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="cursor-pointer flex items-center gap-2 mb-2">
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked
+                                        className=""
+                                    />
+                                    <span className="label-text">Online</span>
+                                </label>
+                            </div>
+                        </div>
+
+                    </article>
+                    <div>
+                    <article className="flex gap-4  ">
+                        <p className="font-bold ">Status</p>
+                        <div>
+                            <div className="form-control">
+                                <label className="cursor-pointer flex items-center gap-2 mb-2">
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked
+                                        className=""
+                                    />
+                                    <span className="label-text">Planning</span>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="cursor-pointer flex items-center gap-2 mb-2">
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked
+                                        className=""
+                                    />
+                                    <span className="label-text">Scheduled</span>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="cursor-pointer flex items-center gap-2 mb-2">
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked
+                                        className=""
+                                    />
+                                    <span className="label-text">Opening</span>
+                                </label>
+                            </div>
+                            <div className="form-control">
+                                <label className="cursor-pointer flex items-center gap-2 mb-2">
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked
+                                        className=""
+                                    />
+                                    <span className="label-text">Completed</span>
+                                </label>
+                            </div>
+                        </div>
+
+                    </article>
+                    </div>
+                </div>
+                <div className="flex items-center gap-20 w-ful mt-[20px]">
+                        <article className="flex gap-3 ml-[15px]">
+            
+                            <p className="font-bold">FSU</p>
+                            <select className="bg-transparent border-2 outline-none  w-[102px] h-[28px]">
+                                        <option>select</option>
+                                    </select>
+                        </article>
+                        <article className="flex gap-3">
+            
+                            <p className="font-bold">Trainer</p>
+                            <select className="bg-transparent border-2 outline-none  w-[102px] h-[28px]">
+                                        <option>select</option>
+                                    </select>
+                        </article>
+                        <article className="flex gap-3">
+            
+                            <p className="font-bold" >Trainer</p>
+                            <select className="bg-transparent border-2 outline-none  w-[102px] h-[28px]">
+                                        <option>select</option>
+                                    </select>
+                        </article>
+                    </div>
+                <section className="flex justify-end items-center">
+                    <Button
+                        title="Clear"
+                        className=" btn bg-[#474747] text-white hover:text-black rounded-lg"
+                        onClick={handleOpenBox}
+                    />
+                    <Button
+                        title="Search"
+                        className="btn bg-primary-color text-white hover:text-black"
+                        onClick={handleAdvancedSearch}
+                    />
+                </section>
             </div>
 
         )
---
+++ src/app/components/class-modal/duplicate-class.tsx
@@ -0,0 +1,81 @@
+import { getClassByID } from "@/services/classes";
+import { Class } from "@/types/class.type";
+import React, { Dispatch, FC, SetStateAction, useEffect, useState } from "react";
+import { Cancel } from "../button/cancel";
+import Button from "../button/button";
+import { toast } from "react-toastify";
+
+type DuplicateClassProp = {
+    classId: number;
+    handleClose: () => void,
+    setdata: Dispatch<SetStateAction<any>>;
+};
+
+export const DuplicateClass: FC<DuplicateClassProp> = ({
+    classId,
+    setdata,
+    handleClose,
+
+}) => {
+
+    const [classByIdToDup, setClassByIdToDup] = useState<Class>({});
+    const getCurrentClassToDup = async (id? : number) => {
+      if (!id) throw new Error("ID is not correct");
+  
+      const response = await getClassByID(id);
+      return response.content;
+    };
+    useEffect(() => {
+        const fetClassById = async () => {
+            const currentClass =  await getCurrentClassToDup(classId);
+            setClassByIdToDup(currentClass)
+            console.log(currentClass)
+        }
+        fetClassById();
+    }, [])
+
+    const handleConfirm = async () => {
+        try {
+            const duplicatedClass=  { ...classByIdToDup}
+            setdata((prevClass: Class[]) => {
+              const currentIndex = prevClass.findIndex(
+                (item) => item.name === classByIdToDup.name
+              );
+              const prevPart = prevClass.slice(0, currentIndex + 1);
+              const nextPart = prevClass.slice(currentIndex + 1);
+              return [...prevPart, duplicatedClass, ...nextPart];
+            })
+            toast.success("Duplicate successful");
+            handleClose();
+            console.log(duplicatedClass)
+        }
+        catch (error) {
+            console.error("Error duplicating class:", error);
+            toast.error("An error occurred while duplicating class");
+           
+        }
+
+    }
+    return (
+        <>
+        <div className="bg-white rounded-lg max-w-md fixed items-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 shadow-lg w-[501px] h-[185px]">
+          <div className="text-[#D45B13] text-[32px] text-center font-bold">Are you sure?</div>
+          <div className="text-[14px] font-bold">
+            <div className="text-center ">
+              Do you really want to DUPLICATE "<span>{classByIdToDup.name}</span>" class?
+            </div>
+            
+
+
+            <div className="flex items-center justify-center space-x-4">
+              <Cancel onClick={handleClose} />
+              <Button title="Confirm" onClick={handleConfirm} />
+
+
+            </div>
+          </div>
+        </div>
+        </>
+    )
+
+}
